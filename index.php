27227b1bb7b1e33720262dd27f02fef7a2b8b38e
abdelkrimnaji <abdelkrim.n@codeur.online>
2018-12-13 10:34:42 +0100

    download

--- index.php
+++ index.php
@@ -1,34 +1,28 @@
 <?php
 
-// require_once("models/connect_bdd.php");
-// require_once("models/film_model.php");
+if (isset($_SERVER["REQUEST_URI"])) {
+	$requete = explode("/", trim($_SERVER['REQUEST_URI'], "/"));
 
-	if (isset($_SERVER["REQUEST_URI"])) {
-
-		$requete = explode("/", trim($_SERVER['REQUEST_URI'], "/"));
-		// print_r($requete);
-		// echo count($requete);
-		$controller = (count($requete) === 1)? "home":$requete[1];
-		$action = (count($requete) < 3)? "log": $requete[2];
-		// $id = (count($requete) < 4)? 0 : intval($requete[3]);
-
-		switch ($controller) {
-			case 'home':
-				require_once("controllers/controller-home.php");
-				break;
-			case 'admin':
-				require_once("controllers/controller-admin.php");
-				break;
-			case 'dashboard':
-				require_once("controllers/controller-dashboard.php");
-				break;			
-			default:
-				require_once("controllers/404.twig");
-				break;
-
-		}
+	$controller = (count($requete) === 1)? "home":$requete[1];
+	$action = (count($requete) < 3)? "log": $requete[2];
+	$id = (count($requete) < 4)? 0 : $requete[3];
+	switch ($controller) {
+		case 'home':
+			require_once("controllers/controller-home.php");
+			break;
+		case 'admin':
+			require_once("controllers/controller-admin.php");
+			break;
+		case 'dashboard':
+			require_once("controllers/controller-dashboard.php");
+			break;	
+		case 'file':
+			require_once("controllers/controller-download.php");
+			break;			
+		default:
+			require_once("controllers/404.twig");
+			break;
 	}
-
-
+}
 
 ?>

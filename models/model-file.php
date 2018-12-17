b861bf8b0b74f9ec523912da801b7669340c36ef
abdelkrimnaji <abdelkrim.n@codeur.online>
2018-12-17 11:41:51 +0100

    fusion

---
+++ models/model-file.php
@@ -0,0 +1,59 @@
+<?php 
+
+require_once('models/connect-bdd.php');
+
+
+function insertFileUpload($arrayDatas) {
+
+	global $bdd;
+
+	$sql = "INSERT INTO `files`(`file_name`, `file_size`, `file_ext`, `folder_key`, `file_key`)
+	 	VALUES (:name, :size, :ext, :folder_key, :file_key)";
+		
+	// Prépare la requête pour éviter les injections SQL...
+	$response = $bdd->prepare( $sql );
+
+	// Bind les paramètres dans la requêtes...
+	$response->bindParam(':name', $arrayDatas["name"], PDO::PARAM_STR);
+	$response->bindParam(':size', $arrayDatas["size"], PDO::PARAM_STR);
+	$response->bindParam(':ext', $arrayDatas["ext"], PDO::PARAM_STR);
+	$response->bindParam(':folder_key', $arrayDatas["folder_key"], PDO::PARAM_STR);
+	$response->bindParam(':file_key', $arrayDatas["file_key"], PDO::PARAM_STR);
+
+	// Exécute la requête...
+    $response->execute();
+    
+}
+
+
+function insertSenderUpload($sender, $message) {
+
+	global $bdd;
+
+	$sql = "INSERT INTO `user_upload`(`mail_sender`, `message`) 
+			VALUES (:sender, :message)";
+
+	// Prépare la requête pour éviter les injections SQL...
+	$response = $bdd->prepare( $sql );		
+
+	// Bind les paramètres dans la requêtes...
+	$response->bindParam(':message', $message, PDO::PARAM_STR);
+	$response->bindParam(':sender', $sender, PDO::PARAM_STR);
+
+	// Exécute la requête...
+    $response->execute();
+}
+
+function getFile_name($fichier) {
+
+    global $bdd;
+
+    $requete = $bdd->prepare('SELECT files.file_name FROM files WHERE files.file_key = :fichier');
+    $requete->bindValue(':fichier', $fichier, PDO::PARAM_STR);
+    $data = $requete->execute();
+    $resultat = $requete->fetch(PDO::FETCH_ASSOC);
+
+    return $resultat;
+}
+
+?>

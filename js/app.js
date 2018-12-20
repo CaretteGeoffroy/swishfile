0f0137edf2b2c4d77981de8a76c2d6034ccec954
GeoffroyCarette <geoffroy.c@codeur.online>
2018-12-20 10:32:56 +0100

    Ajout d'une alerte si aucun fichiers upload

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
 let input = document.getElementById('file-input');
 let infoArea = document.getElementById('file-upload-filename');
 const imgUpload = document.querySelector('.imgUpload');
+const myButton = document.querySelector('.myButton');
 let i;
+let correct = false;
 
 input.addEventListener('change', showFileName);
+myButton.addEventListener('click', verifInputValue);
 
 // Permet l'affichage des fichiers insérés avec leurs infos (nom, taille, extention)
+
+
+
 function showFileName(event) {
 
     // Fait disparaitre l'image d'upload
     imgUpload.style.display = 'none';
+
+    correct = true;
 
     for (i = 0; i < input.files.length; i++) {
 
@@ -30,6 +38,11 @@ function showFileName(event) {
         // Injecte les infos récupérés dans la div ciblé
         infoArea.innerHTML += '<p class="files_content d-block mx-auto">' + fileName + '</p>';
         infoArea.innerHTML += '<p class="files_attribute d-block mx-auto">' + fileSize + ' ko' + ' | ' + getExt + '</p>';
+
+        // Affecte une valeur a l'input
+        
+
+        
     }
 }
 
@@ -44,3 +57,15 @@ function copyLink() {
   
     alert("Le lien a été copié dans le presse-papier !");
   }
+
+
+function verifInputValue() {
+
+    // Si la valeur de l'input n'est pas défini -> Retourne une alerte
+    if (!correct) {
+
+        swal("Attention !", "Merci d'ajouter vos fichiers avant d'envoyer !", "warning");
+    }
+}
+
+
